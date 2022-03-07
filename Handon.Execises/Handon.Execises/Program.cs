603bd9dce5e161abc0d3d18ea2bd6dfb97534eec
辻敬太 <k.tsuji@infocom.co.jp>
2022-03-07 15:11:10 +0900

    init

---
+++ Handon.Execises/Handon.Execises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Handon.Execises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
